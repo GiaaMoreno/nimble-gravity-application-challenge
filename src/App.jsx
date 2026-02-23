010c52d5fffd56f048ca38feb063ecffaf4d9c8f
GianinaMoreno <gianinamoreno92@gmail.com>
2026-02-23 03:15:18 -0300

    Add candidate information about jobs offers

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,21 @@ function App() {
   
   return(
     <div style={{padding: "2rem"}}>
+
+      {candidate && (
+        <div
+          style={{marginBottom: "2rem", padding:"1rem", backgroundColor:"#f5f5f5", borderRadius:"8px"}}
+        > 
+          <h2>
+            Applying as: {candidate.firstName} {candidate.lastName}
+          </h2>
+          <p>{candidate.email}</p>
+          <p>ID: {candidate.candidateId} </p>
+          <p>UUID: {candidate.uuid}</p>
+        </div>
+      )}
+
+
       <h1> Available Positions </h1>
 
       <JobList jobs={jobs} candidate={candidate}/>

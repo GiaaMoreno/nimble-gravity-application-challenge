00230132754dd519949976b80dbc92d85dea802d
GianinaMoreno <gianinamoreno92@gmail.com>
2026-02-23 02:02:16 -0300

    Implement candidate and jobs

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { getCandidateByEmail, getJobs } from './services/api'
+import JobList from "./components/JobList"
+//import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [candidate, setCandidate] = useState(null);
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const email = "gianinamoreno92@gmail.com"
+
+  useEffect(() => {
+    const fetchData = async () => {  
+      try {
+        const candidateData = await getCandidateByEmail(email);
+        //console.log("Candidate: ", candidateData);
+
+        const jobsData = await getJobs();
+        //console.log("Jobs: ", jobsData);
+
+        setCandidate(candidateData);
+        setJobs(jobsData);
+     
+      } catch (err) {
+        //console.error("Error fetching candidate:", err);
+        
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
+  if(loading) return <p> Loading candidate data... </p>;
+  if(error) return <p> Error: {error}</p>;
+  
+  return(
+    <div style={{padding: "2rem"}}>
+      <h1> Available Positions </h1>
+
+      <JobList jobs={jobs} candidate={candidate}/>
+        
+    </div>
+  
+          
+  );
 }
 
-export default App
+export default App;
